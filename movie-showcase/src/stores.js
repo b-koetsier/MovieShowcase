07839f5534a610735b48d6597ec0141c1c20a605
b-koetsier <brian@securancy.com>
2024-09-16 16:19:27 +0200

    added type definition

--- movie-showcase/src/stores.js
+++ movie-showcase/src/stores.js
@@ -1,5 +1,24 @@
 import { writable } from 'svelte/store';
 
+/**
+ * @typedef {Object} Movie
+ * @property {string} Title
+ * @property {number} Year
+ * @property {string} Plot
+ * @property {string} Poster
+ */
+
+/**
+ * @type {import('svelte/store').Writable<Movie[]>}
+ */
 export const movieArray = writable([]);
+
+/**
+ * @type {import('svelte/store').Writable<number>}
+ */
 export const slideDuration = writable(5);
+
+/**
+ * @type {import('svelte/store').Writable<number>}
+ */
 export const carouselItemCount = writable(1);
